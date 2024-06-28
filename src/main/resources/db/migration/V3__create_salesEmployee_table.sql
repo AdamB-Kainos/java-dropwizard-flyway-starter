7f0ad13d5fd23ec72891436900fa0ba2b8f1c2d8
Adam Bowman <adam.bowman@kainos.com>
2024-06-28 09:54:01 +0100

    fixed error

--- src/main/resources/db/migration/V3__create_salesEmployee_table.sql
+++ src/main/resources/db/migration/V3__create_salesEmployee_table.sql
@@ -3,7 +3,7 @@ CREATE TABLE salesEmployee (
     firstName varchar(200) NOT NULL,
     lastName varchar(200) NOT NULL,
     salary decimal NOT NULL,
-    bankAccountNumber int(50) NOT NULL,
-    nationalInsuranceNumber int(20) NOT NULL,
+    bankAccountNumber varchar(20) NOT NULL,
+    nationalInsuranceNumber varchar(20) NOT NULL,
     commissionRate int(30) NOT NULL
 );
---
+++ src/main/resources/db/migration/V7__InsertDataToTables.sql
@@ -0,0 +1,31 @@
+INSERT INTO salesEmployee (firstName, LastName, salary, bankAccountNumber, nationalInsuranceNumber, commissionRate)
+    VALUES ('Adam','Bowman',100000.00, 12345678, 11111111, 10),
+     ('John','Smith',50000.00, 22345678, 22222222, 10),
+     ('Andrew','Brown',200000.00, 3245678, 33333333, 10),
+     ('Thomas','Yellow',30000.00, 42345678, 44444444, 10);
+
+     INSERT INTO deliveryEmployee(fname,lname,salary,bankAccountNum,nationalInsureNum) Values ('Bob','Smith',22000,'1234567890','ABCD56TD');
+     INSERT INTO deliveryEmployee(fname,lname,salary,bankAccountNum,nationalInsureNum) Values ('Sam','Smith',36000,'0987654321','LKJK56TA');
+     INSERT INTO deliveryEmployee(fname,lname,salary,bankAccountNum,nationalInsureNum) Values ('Bill','Barker',99000,'1324354657','UYHV76YK');
+     INSERT INTO deliveryEmployee(fname,lname,salary,bankAccountNum,nationalInsureNum) Values ('Phil','Smith',78000,'0981234567','BHUT98TR');
+     INSERT INTO deliveryEmployee(fname,lname,salary,bankAccountNum,nationalInsureNum) Values ('Tracy','Smith',35000,'1234567890','NBVC76UY');
+
+     INSERT INTO client (name, address, phoneNumber)
+     VALUES ('FirstClient', '1 Client Street, Belfast', '07812345678');
+
+     INSERT INTO client (name, address, phoneNumber)
+     VALUES ('SecondClient', '2 University Street, Belfast', '07812375678');
+
+     INSERT INTO client (name, address, phoneNumber)
+     VALUES ('ThirdClient', '3 Church Hill, Ballygowan', '07812375835');
+
+     INSERT INTO client (name, address, phoneNumber)
+     VALUES ('FourthClient', '4 Hanwood Drive Street, Belfast', '07812379268');
+
+INSERT INTO Project(ProjectID, ProjectName, Value, Technologies, TechLead, clientID)
+       VALUES (1,'Database Project',5155.00,'mySQL',1, 1),
+       (2,'Programming Project',4120.30,'Java', 2, 2),
+       (3,'API Project',11545.89,'Maven', 3, 3),
+       (4,'Website Creation',2400.50,'JavaScript', 4, 4);
+
+
