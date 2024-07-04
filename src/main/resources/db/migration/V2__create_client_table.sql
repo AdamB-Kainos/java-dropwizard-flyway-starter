d2b4f17051957d839b5bc6245342dbd7b168bf09
Adam Bowman <adam.bowman@kainos.com>
2024-07-04 10:15:30 +0100

    Deleted migrate

--- src/main/resources/db/migration/V2__create_client_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE client (
-    clientID int PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    name varchar(100) NOT NULL,
-    address varchar (200) NOT NULL,
-    phoneNumber varchar (20)
-);
--- src/main/resources/db/migration/V3__create_salesEmployee_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE salesEmployee (
-    employeeID int PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    firstName varchar(200) NOT NULL,
-    lastName varchar(200) NOT NULL,
-    salary decimal NOT NULL,
-    bankAccountNumber varchar(20) NOT NULL,
-    nationalInsuranceNumber varchar(20) NOT NULL,
-    commissionRate int(30) NOT NULL
-);
